eb015bb909ba9d1c38c4696383d71cc54186b9d4
Jose Ordaz <145297953+Odz17@users.noreply.github.com>
2024-03-11 22:10:31 +0100

    cloudinary

--- routes/restaurants.routes.js
+++ routes/restaurants.routes.js
@@ -63,7 +63,7 @@ router.post("/upload", fileUploader.single("image"), (req, res) => {
 });
 
 // ruta final localhost:5005/restaurants/create, importante recordar la ruta!!!!!!!!!!!
-router.post("/create", isAuthenticated, fileUploader.single("image"), (req, res) => {
+router.post("/create", isAuthenticated, (req, res) => {
   console.log(req.body); // Verifica los datos del formulario
   console.log(req.file);
   const owner = req.payload;
@@ -91,7 +91,7 @@ router.post("/create", isAuthenticated, fileUploader.single("image"), (req, res)
     price: req.body.price,
     description: req.body.description,
     category: req.body.category,
-    image: req.file ? req.file.path : undefined,
+    image: req.body.image,
     phone: req.body.phone,
     owner: owner._id,
   })
