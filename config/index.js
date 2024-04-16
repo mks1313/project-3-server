db6d7231067bebda0e27eb36e1f0548dec888f2b
mks1313 <139466229+mks1313@users.noreply.github.com>
2024-04-16 22:06:24 +0200

    probando-desplegue

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      credentials: true,
+      // credentials: true,
       origin: process.env.ORIGIN || 'http://localhost:5173'
     })
   );
